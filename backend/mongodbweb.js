b5de6bed8e6690eac91189fb6216000221271c09
d4thomas <152221911+d4thomas@users.noreply.github.com>
2025-08-18 13:48:22 -0400

    chore: add comments to mongodbweb.js

--- backend/mongodbweb.js
+++ backend/mongodbweb.js
@@ -1,10 +1,13 @@
+// Import required libraries
 import { createServer } from 'node:http'
 import { MongoClient } from 'mongodb'
 
+// Create MongoDB client
 const url = 'mongodb://localhost:27017/'
 const dbName = 'cs618'
 const client = new MongoClient(url)
 
+// Connect to MongoDB
 try {
   await client.connect()
   console.log('Successfully connected to database!')
@@ -12,17 +15,22 @@ try {
   console.error('Error connecting to database:', err)
 }
 
+// Create HTTP server
 const server = createServer(async (req, res) => {
+  // Select database from client
   const db = client.db(dbName)
   const users = db.collection('users')
 
+  // Save collection to array
   const usersList = await users.find().toArray()
 
+  // Set status code/header and return collection array
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify(usersList))
 })
 
+// Start HTTP server
 const host = 'localhost'
 const port = 3000
 server.listen(port, host, () => {
