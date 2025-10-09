59b74938e6001cff0cc9027b35b1af0e3a7fc459
d4thomas <152221911+d4thomas@users.noreply.github.com>
2025-10-09 15:31:11 -0400

    chore: add server-side rendering/data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Blog } from './pages/Blog'
-import { Signup } from './pages/Signup'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
-import { Login } from './pages/Login.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Blog />,
-  },
-  {
-    path: '/signup',
-    element: <Signup />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-])
+import PropTypes from 'prop-types'
 
 const queryClient = new QueryClient()
 
-export function App() {
+export function App({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <RouterProvider router={router} />
-      </AuthContextProvider>
+      <AuthContextProvider>{children}</AuthContextProvider>
     </QueryClientProvider>
   )
+}
+
+App.propTypes = {
+  children: PropTypes.element.isRequired,
 }
